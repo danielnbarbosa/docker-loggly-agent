d22a53d89a89e9e80be074912590303ca02d5177
Daniel Barbosa <danielnbarbosa@yahoo.com>
2014-11-20 18:19:37 -0800

    naming cleanup

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 SHA1=$1
 
 # Deploy image to Docker Hub
-#docker push danielnbarbosa/docker-loggly-agent:$SHA1
-docker push danielnbarbosa/docker-loggly-agent
+#docker push danielnbarbosa/loggly-agent:$SHA1
+docker push danielnbarbosa/loggly-agent
 
 # Deploy to CoreOS cluster
 ssh core@54.67.14.65 -C 'fleetctl stop loggly-agent@{1..3}.service; fleetctl start loggly-agent@{1..3}.service'
